badfcbbc39b6ad6657aebed9d665f448c99f7a5b
Trung Nguyen <trung.n.k@gmail.com>
2019-07-15 13:52:53 -0400

    start specific node

--- examples/7nodes/raft-start.sh
+++ examples/7nodes/raft-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -u
 set -e
+permissioned=(_ yes yes yes yes no no no)
 
 function usage() {
   echo ""
@@ -13,6 +14,29 @@ function usage() {
   echo ""
   ./tessera-start.sh --help
   exit -1
+}
+
+NETWORK_ID=$(cat genesis.json | tr -d '\r' | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
+
+if [ $NETWORK_ID -eq 1 ]
+then
+    echo "  Quorum should not be run with a chainId of 1 (Ethereum mainnet)"
+    echo "  please set the chainId in the genensis.json to another value "
+    echo "  1337 is the recommend ChainId for Geth private clients."
+fi
+
+mkdir -p qdata/logs
+
+function startNode() {
+	node=$1
+
+	echo "[*] Start node $node"
+	QUORUM_GETH_ARGS=${QUORUM_GETH_ARGS:-}
+	ARGS="--nodiscover --verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints $QUORUM_GETH_ARGS"
+	if [[ "${permissioned[node]}" == "yes" ]]; then
+		ARGS="$ARGS --permissioned"
+	fi
+	PRIVATE_CONFIG=qdata/c$node/tm.ipc nohup geth --datadir qdata/dd$node $ARGS --raftport $((50400 + $node)) --rpcport $((22000 + $node - 1)) --port $((21000 + $node - 1)) --unlock 0 --password passwords.txt 2>>qdata/logs/$node.log &
 }
 
 privacyImpl=tessera
@@ -39,6 +63,10 @@ while (( "$#" )); do
             shift
             usage
             ;;
+		[1-7])
+			startNode $1
+			exit 0
+			;;
         *)
             echo "Error: Unsupported command line parameter $1"
             usage
@@ -46,16 +74,6 @@ while (( "$#" )); do
     esac
 done
 
-NETWORK_ID=$(cat genesis.json | tr -d '\r' | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
-
-if [ $NETWORK_ID -eq 1 ]
-then
-	echo "  Quorum should not be run with a chainId of 1 (Ethereum mainnet)"
-    echo "  please set the chainId in the genensis.json to another value "
-	echo "  1337 is the recommend ChainId for Geth private clients."
-fi
-
-mkdir -p qdata/logs
 
 if [ "$privacyImpl" == "tessera" ]; then
   echo "[*] Starting Tessera nodes"
@@ -72,16 +90,11 @@ else
 fi
 
 echo "[*] Starting Ethereum nodes with ChainID and NetworkId of $NETWORK_ID"
-QUORUM_GETH_ARGS=${QUORUM_GETH_ARGS:-}
 set -v
-ARGS="--nodiscover --verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints $QUORUM_GETH_ARGS"
-PRIVATE_CONFIG=qdata/c1/tm.ipc nohup geth --datadir qdata/dd1 $ARGS --permissioned --raftport 50401 --rpcport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/1.log &
-PRIVATE_CONFIG=qdata/c2/tm.ipc nohup geth --datadir qdata/dd2 $ARGS --permissioned --raftport 50402 --rpcport 22001 --port 21001 --unlock 0 --password passwords.txt 2>>qdata/logs/2.log &
-PRIVATE_CONFIG=qdata/c3/tm.ipc nohup geth --datadir qdata/dd3 $ARGS --permissioned --raftport 50403 --rpcport 22002 --port 21002 --unlock 0 --password passwords.txt 2>>qdata/logs/3.log &
-PRIVATE_CONFIG=qdata/c4/tm.ipc nohup geth --datadir qdata/dd4 $ARGS --permissioned --raftport 50404 --rpcport 22003 --port 21003 --unlock 0 --password passwords.txt 2>>qdata/logs/4.log &
-PRIVATE_CONFIG=qdata/c5/tm.ipc nohup geth --datadir qdata/dd5 $ARGS --raftport 50405 --rpcport 22004 --port 21004 --unlock 0 --password passwords.txt 2>>qdata/logs/5.log &
-PRIVATE_CONFIG=qdata/c6/tm.ipc nohup geth --datadir qdata/dd6 $ARGS --raftport 50406 --rpcport 22005 --port 21005 --unlock 0 --password passwords.txt 2>>qdata/logs/6.log &
-PRIVATE_CONFIG=qdata/c7/tm.ipc nohup geth --datadir qdata/dd7 $ARGS --raftport 50407 --rpcport 22006 --port 21006 --unlock 0 --password passwords.txt 2>>qdata/logs/7.log &
+for i in {1..7}
+do
+	startNode $i
+done
 set +v
 
 echo
